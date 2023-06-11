60981923f6166d03c4952ade5bb7783b00a79acb
tineggarcia <tineggarcia@gmail.com>
2023-06-11 02:11:20 +0100

    Add requirements.tx and code imports

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+A simple game to test your vocabulary skills
+by unscrambling the hidden antonym word
+"""
+
+from PyDictionary import PyDictionary
+dictionary=PyDictionary()
+
+
