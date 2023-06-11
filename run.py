172029975f53239575939ab0e9a3c4a9dfae52bf
tineggarcia <tineggarcia@gmail.com>
2023-06-11 05:44:41 +0100

    Added function to get word meaning from PyDictionary

--- run.py
+++ run.py
@@ -10,18 +10,19 @@ dictionary=PyDictionary()
 """
 main function, holds the dictionary
 """
-def get_opposite_word_lists():
-    #An array of words, second word will be looked through PyDictionary
-    opposites_list = [ ["humble", "proud"],
-                 ["efficient", "ineffective"],
-                 ["smart", "stupid"],
-                 ["beautiful", "hideous"],
-                 ["scrumptious", "inedible"],
-                 ["frugal", "extravagant"],
-                 ["ridiculous", "sensible"],
-                 ["furious", "calm"] ]
 
-    return opposites_list
+def _init():
+    #An array of words, second word will be looked through PyDictionary
+    WORDLIST = [ ["humble", "proud", get_word_meaning("proud", "proud")],
+                 ["efficient", "ineffective", get_word_meaning("ineffective", "ineffective")],
+                 ["smart", "stupid", get_word_meaning("stupid", "stupid")],
+                 ["beautiful", "hideous", get_word_meaning("hideous", "hideous")],
+                 ["scrumptious", "inedible", get_word_meaning("inedible", "inedible")],
+                 ["frugal", "extravagant", get_word_meaning("extravagant", "extravagant")],
+                 ["ridiculous", "sensible", get_word_meaning("sensible", "sensible")],
+                 ["furious", "calm", get_word_meaning("calm", "calm")]]
+
+    return WORDLIST
 
 """
 Function to scramble words, passing word argument. 
@@ -39,7 +40,7 @@ def jumble_word(word_arg):
     jumbled_word = ''.join(jumbled_char)
     return jumbled_word
 
- #Generate array of random numbers without DUPLICATES
+#Generate array of random numbers without DUPLICATES
 def generate_random_word_idx(no_of_words):
     random_idx = []
     for w in range(0,no_of_words):
@@ -54,15 +55,25 @@ def generate_random_word_idx(no_of_words):
             counter += 1
     return random_idx
 
+def get_word_meaning(word, default_meening):
+    word_meaning = default_meening
+    meanings = dictionary.meaning(word)
+    if meanings != None:
+        meaning = list(meanings.values())[0][0]
+    return meaning
+
 def main():
 
-    word_list = get_opposite_word_lists()
-    orig_word = word_list[0][0]
+    word_list = _init()
+    for word in word_list:
+        print(word)
+
+    orig_word = word_list[3][0]
     jumbled_word = jumble_word(orig_word)
-    opposite_word = word_list[0][1]
-    print(orig_word + " = " + jumbled_word + "not " + opposite_word)
-    # print(jumble_word("armageddon"))
-    # print(generate_random_word_idx(10))
+    opposite_word = word_list[3][1]
+    opposite_def = get_word_meaning(opposite_word, opposite_word)
+    print(orig_word + " is " + jumbled_word + " not " + opposite_word)
+    print(opposite_def)
 
 
 main()
